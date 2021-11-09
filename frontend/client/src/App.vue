a744c94e36f7348e41a532a1b32ccf689c8142ce
chinadupaya <annysiad@yahoo.com>
2021-11-09 19:43:10 +0800

    Add Adobe Clean font

--- frontend/client/src/App.vue
+++ frontend/client/src/App.vue
@@ -19,8 +19,9 @@
 </template>
 
 <style>
+@import url('http://fonts.cdnfonts.com/css/adobe-clean');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Adobe Clean', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;

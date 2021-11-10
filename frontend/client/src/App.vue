33f3f1eddbc61cce0ae3a0fcdb01813acbfb1523
chinadupaya <annysiad@yahoo.com>
2021-11-10 20:32:41 +0800

    Update navbar icon, 404 page, and footer

--- frontend/client/src/App.vue
+++ frontend/client/src/App.vue
@@ -1,51 +1,53 @@
 <template>
-	<div id="app">
+	<div id="app" class="d-flex flex-column min-vh-100">
 		<div id="nav">
 		<b-navbar toggleable="lg">
-		<b-navbar-brand href="#"><img src="img/icons/mayari-white.png" style="width:50; height:50px"/></b-navbar-brand>
+		<b-navbar-brand href="/404"><img src="img/icons/mayari-white-shadow.png" style="width:50; height:50px"/></b-navbar-brand>
 
 		<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+		<b-collapse id="nav-collapse" is-nav>
 			<b-navbar-nav> 
 				<b-nav-item class="nav-style" to="/">Home</b-nav-item>
-				<b-nav-item class="nav-style" to="#">FAQ</b-nav-item>
-				<b-nav-item class="nav-style" to="#">Blog</b-nav-item>
+				<b-nav-item class="nav-style" to="/404">FAQ</b-nav-item>
+				<b-nav-item class="nav-style" to="/404">Blog</b-nav-item>
 				<b-nav-item class="nav-style" to="/about">About Us</b-nav-item>
-				<b-nav-item class="nav-style" to="#">Contact Us</b-nav-item>
+				<b-nav-item class="nav-style" to="/404">Contact Us</b-nav-item>
 			</b-navbar-nav>
+		</b-collapse>
 		</b-navbar>
 		</div>
 		<router-view />
 		<!-- FOOTER -->
-		<div class="section-footer">
+		<footer class="section-footer mt-auto">
 			<b-container>
 				<b-row>
-					<b-col>
+					<b-col xs="6">
 						<h3>MAYARI</h3>
 						<span>Quezon City, Metro Manila</span> <br/>
 						<span>Philippines, 1126</span> <br/>
 						<span>support@mayari.com</span> <br/>
 						<span>+63 (917) 000 0000</span>
 					</b-col>
-					<b-col>
+					<b-col xs="6">
 						<h4>BORROW</h4>
-						<b-link class="footer-link" to="#">Business Loans</b-link>
-						<b-link class="footer-link" to="#">Personal Loans (Coming Soon)</b-link>
-						<b-link class="footer-link" to="#">Finance Loans (Coming Soon)</b-link>
+						<b-link class="footer-link" to="/404">Business Loans</b-link>
+						<b-link class="footer-link" to="/404">Personal Loans (Coming Soon)</b-link>
+						<b-link class="footer-link" to="/404">Finance Loans (Coming Soon)</b-link>
 					</b-col> 
-					<b-col>
+					<b-col xs="6">
 						<h4>LEARN</h4>
-						<b-link class="footer-link" to="#">Home</b-link>
-						<b-link class="footer-link" to="#">Blog</b-link>
-						<b-link class="footer-link" to="#">FAQ</b-link>
+						<b-link class="footer-link" to="/404">Home</b-link>
+						<b-link class="footer-link" to="/404">Blog</b-link>
+						<b-link class="footer-link" to="/404">FAQ</b-link>
 					</b-col> 
-					<b-col>
+					<b-col xs="6">
 						<h4>CONNECT</h4>
-						<b-link class="footer-link" to="#">About Us</b-link>
-						<b-link class="footer-link" to="#">Contact Us</b-link>
+						<b-link class="footer-link" to="/404">About Us</b-link>
+						<b-link class="footer-link" to="/404">Contact Us</b-link>
 					</b-col> 
 				</b-row>
 			</b-container>
-		</div>
+		</footer>
 	</div>
 </template>
 
@@ -72,8 +74,15 @@
 #nav a.router-link-exact-active {
 	color: #000000;
 }
+#nav a.nav-link:hover {
+	background-color: #7070701c;
+	border-radius: 10px;
+}
+#nav a.router-link-exact-active {
+	color: #000000;
+}
 /* FOOTER */
-div.section-footer {
+footer.section-footer {
 	padding: 1em 1em ;
 	color: white;
 	text-align: left;

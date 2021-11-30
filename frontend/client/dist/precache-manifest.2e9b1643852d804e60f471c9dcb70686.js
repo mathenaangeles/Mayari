99825caf347171bc8acef7fe4ef8c9ee36af862b
mathena828 <mathena828@gmail.com>
2021-11-30 18:11:27 +0800

    AWS S3 deployment

---
+++ frontend/client/dist/precache-manifest.2e9b1643852d804e60f471c9dcb70686.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fbe4fe582702498dbcd",
+    "url": "/css/app.ecc1e919.css"
+  },
+  {
+    "revision": "4198bde15b78f5e1046e",
+    "url": "/css/chunk-vendors.57b5595f.css"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/favicon.ico:Zone.Identifier"
+  },
+  {
+    "revision": "39f9b8fc4b613315d89cdaf1795505b0",
+    "url": "/img/hero_circle_2.39f9b8fc.png"
+  },
+  {
+    "revision": "3b99345238e6169f35cace976ba4ffe6",
+    "url": "/img/hero_circle_3.3b993452.png"
+  },
+  {
+    "revision": "00ff662a61e4846ec6c1a87e67885004",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fbe4fe582702498dbcd",
+    "url": "/js/app.b616d951.js"
+  },
+  {
+    "revision": "4198bde15b78f5e1046e",
+    "url": "/js/chunk-vendors.01d2a037.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "9a7554bd1cc5fb63518e667f1690ea93",
+    "url": "/mayari-icon.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);

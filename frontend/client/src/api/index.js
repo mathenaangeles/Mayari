2eebd74affd39fad80b79e4cc4a2a159dbb5d391
mathena828 <mathena828@gmail.com>
2022-01-07 02:34:48 +0800

    added loan HTTP requests

--- frontend/client/src/api/index.js
+++ frontend/client/src/api/index.js
@@ -2,9 +2,17 @@ import axios from "axios";
 
 const API_URL = "http://127.0.0.1:5000";
 
-// export function fetchLoans() {
-//   return axios.get(`${API_URL}/loans/`, {headers:{ Authorization: `Bearer: ${jwt}`}})
-// }
+export function fetchLoans(userId, jwt) {
+  return axios.get(`${API_URL}/loans/${userId}`, { headers:{ Authorization: `Bearer: ${jwt}`} })
+}
+
+export function fetchLoan (loanId, jwt) {
+  return axios.get(`${API_URL}/loans/${loanId}/`, { headers:{ Authorization: `Bearer: ${jwt}`} })
+}
+
+export function postLoan (loan, jwt) {
+  return axios.post(`${API_URL}/surveys/`, loan, { headers: { Authorization: `Bearer: ${jwt}` } })
+}
 
 export function login(user) {
   return axios.post(`${API_URL}/users/login/`, user);

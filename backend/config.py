f35cb3ca3bf6170f896b3e524b22df30d93dbe20
chinadupaya <annysiad@yahoo.com>
2022-02-11 18:35:59 +0800

    Update flask env variables

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,9 @@
+import os
 class BaseConfig(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///mayari.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'whorunthehelloworld'
+    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    REGION_NAME = os.environ.get('REGION_NAME')
--- backend/utils.py
+++ backend/utils.py
@@ -20,19 +20,22 @@ def token_required(f):
             'message': 'Expired token...',
             'authenticated': False
         }
+        missing_headers = {
+            'message': 'Invalid headers...',
+            'authenticated': False
+        }
         if len(auth_headers) != 2:
-            return jsonify(invalid_msg), 401
+            return jsonify(missing_headers), 400
         try:
             token = auth_headers[1]
-            data = jwt.decode(token, current_app.config['SECRET_KEY'],  algorithms=['HS256'])
+            data = jwt.decode(token, 'whorunthehelloworld',  algorithms=['HS256'])
             user = User.query.filter_by(email=data['sub']).first()
             if not user:
                 raise RuntimeError('User could not be found.')
             return f(user, *args, **kwargs)
         except jwt.ExpiredSignatureError:
             return jsonify(expired_msg), 401 
-        except (jwt.InvalidTokenError, Exception) as e:
-            print(e)
+        except (jwt.InvalidTokenError) :
             return jsonify(invalid_msg), 401
     return _verify
 
@@ -66,7 +69,10 @@ def admin_token_required(f):
     return _verify
 
 def upload_file(file, bucket, key):
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client('s3',
+        aws_access_key_id=current_app.config['AWS_ACCESS_KEY'],
+        aws_secret_access_key=current_app.config['AWS_SECRET_ACCESS_KEY'],
+        region_name = current_app.config['REGION_NAME'])
     s3_client.upload_fileobj(file, bucket, key)
     file_url = 'https://%s.s3.amazonaws.com/%s' % (bucket, key)
     return file_url

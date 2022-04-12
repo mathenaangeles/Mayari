263c1eda07fc46e5fd0b77ec35796731bbf52c10
chinadupaya <annysiad@yahoo.com>
2022-04-12 17:43:32 +0800

    Clean up UI for admin dashboard and loan

--- frontend/client/src/views/admin/AdminDashboard.vue
+++ frontend/client/src/views/admin/AdminDashboard.vue
@@ -1,22 +1,54 @@
 <template>
   <div class="admin-dashboard">
     <h1>Admin</h1>
-    <b-table striped hover :items="loans"></b-table>
-    <div v-for="loan in loans" v-bind:key="loan.id" class="admin-loan-card">
-      <p>{{ loan.id }}</p>
-      <router-link :to="`loan/${loan.id}`">See Loan</router-link>
-    </div>
+    <b-table striped hover :fields="fields" :items="loans">
+
+      <template #cell(id)="data">
+        <!-- `data.value` is the value after formatted by the Formatter -->
+        {{ data.item.id }}
+        <router-link :to="`loan/${data.item.id}`" v-b-tooltip.hover title="Edit Loan Info"> 
+          <Pencil />
+        </router-link>
+      </template>
+    </b-table>
   </div>
 </template>
-<style scoped></style>
+<style scoped>
+  .admin-dashboard {
+    overflow: scroll;
+  }
+</style>
 <script>
 import { mapState } from "vuex";
+import Pencil from "vue-material-design-icons/Pencil.vue";
 export default {
   name: "AdminDashboard",
-  components: {},
+  components: {
+    Pencil
+  },
   computed: mapState({
     loans: (state) => state.loans,
   }),
+  data() {
+    return {
+      fields: [
+        'id',
+        'borrower_id',
+        'collateral',
+        'collateral_type',
+        'created_at',
+        'installments',
+        'interest_rate',
+        'outstanding_balance',
+        'overdue_balance',
+        'payment_term',
+        'principal',
+        'requested_amount',
+        'status',
+        'total_amount',
+      ]
+    }
+  },
   beforeMount() {
     this.$store.dispatch("fetchAllLoans");
   },

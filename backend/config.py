39316f21c8608a0165bc3640f29a458a162ed106
chinadupaya <annysiad@yahoo.com>
2022-04-27 20:35:58 +0800

    Connect flask to postgres container

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,12 @@
 import os
+user = os.environ['POSTGRES_USER']
+password = os.environ['POSTGRES_PASSWORD']
+host = os.environ['POSTGRES_HOST']
+database = os.environ['POSTGRES_DB']
+port = os.environ['POSTGRES_PORT']
 class BaseConfig(object):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///mayari.db' # os.getenv("DATABASE_URL", "sqlite://") # 
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{user}:{password}@{host}:{port}/{database}' # 'sqlite:///mayari.db' # 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'whorunthehelloworld'
     AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')

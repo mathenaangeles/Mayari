d2f8912eb085065671b617b32a53bb879c52aa40
chinadupaya <annysiad@yahoo.com>
2022-06-07 18:45:31 +0800

    Add static filter for Blog Dashboard

--- frontend/client/src/views/Blog.vue
+++ frontend/client/src/views/Blog.vue
@@ -1,5 +1,45 @@
 <template>
   <div class="blog">
+  <b-row class="px-5">
+    <b-col md="9" class="d-flex justify-content-around">
+      <b-button
+        variant="light"
+        >All Categories</b-button
+       >
+      <b-button
+        variant="light"
+        >Financial Literacy</b-button
+        >
+      <b-button
+        variant="light"
+        >Personal Finance</b-button
+        >
+      <b-button
+        variant="light"
+        >Financial Wellness</b-button
+      >
+      <b-button
+        variant="light"
+        >Financial Planning</b-button
+      >
+    </b-col>
+    <b-col md="3">
+      <b-row class="search-container">
+        <b-col cols="10" class="pr-0">
+          <b-form-input 
+            placeholder="Search for keyword..."
+            type="search"
+            class="custom-search pr-0">
+          </b-form-input>
+        </b-col>
+        <b-col cols="1" class="px-0">
+          <b-button variant="light" class="search-button">
+            <Magnify/>
+          </b-button>
+        </b-col>
+      </b-row>
+    </b-col>
+  </b-row>
     <div v-if="!articles || articles.length === 0">
       <h2>Oops, there's nothing here!</h2>
     </div>
@@ -11,14 +51,33 @@
     </BlogDashboardItem>
   </div>
 </template>
-<style scoped></style>
+<style scoped>
+.search-container {
+  border: 2px solid #7070701c;
+  border-radius: 10px
+
+}
+.custom-search {
+  border: 0;
+  outline: 0;
+}
+.custom-search:focus {
+  box-shadow: none;
+}
+
+.search-button {
+  background-color: white;
+}
+</style>
 <script>
+import Magnify from "vue-material-design-icons/Magnify";
 import { mapState } from "vuex";
 import BlogDashboardItem from "@/components/BlogDashboardItem.vue";
 export default {
   name: "Blog",
   components: {
     BlogDashboardItem,
+    Magnify
   },
   computed: mapState({
     articles: (state) => state.articles,
